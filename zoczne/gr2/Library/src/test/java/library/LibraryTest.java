771105e6ca7a6043b237e513fa0baa636541a730
kpodlaski <podlaski@uni.lodz.pl>
2020-03-07 15:40:09 +0100

    gr 2 07.03.2020, v3

--- zoczne/gr2/Library/src/test/java/library/LibraryTest.java
+++ zoczne/gr2/Library/src/test/java/library/LibraryTest.java
@@ -4,6 +4,9 @@ import library.model.Author;
 import library.model.Book;
 import library.model.BookDetails;
 
+import java.lang.reflect.Field;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 public class LibraryTest {
@@ -16,11 +19,15 @@ public class LibraryTest {
         Author a = new Author("Stanisław", "Lem");
         BookDetails bd = new BookDetails(a, "Solaris");
         Book b = new Book(bd,1);
-        lib.addBook(b);
-        lib.addBook(new Book(bd, 2));
+        Field booksField = Library.class.getDeclaredField("books");
+        booksField.setAccessible(true);//Turn off privacy
+        List<Book> books = (List<Book>) booksField.get(lib);
+        books.add(b);
+        books.add(new Book(bd, 2));
         a = new Author("Władysław", "Reymont");
         bd = new BookDetails(a, "Chłopi");
-        lib.addBook(new Book(bd,3));
+        books.add(new Book(bd,3));
+        booksField.setAccessible(false);
     }
 
     @org.junit.After
@@ -36,10 +43,13 @@ public class LibraryTest {
         //check if id == 2
         assertEquals(2, book.getId());
         //check number of authors
-        assertEquals(2,book.getAuthors().size());
+        assertEquals(1,book.getAuthors().size());
         //check Author name/surname
         assertEquals("Stanisław",book.getAuthors().get(0).getName());
         assertEquals("Lem",book.getAuthors().get(0).getSurname());
+        //Equivalent author testing
+        Author a = new Author("Stanisław","Lem");
+        assertEquals(a,book.getAuthors().get(0));
         //Check title
         assertEquals("Solaris",book.getTitle());
     }
